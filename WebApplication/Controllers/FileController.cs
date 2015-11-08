223bae76fe56f436a684185720569f6681710de2
garafu <garafo@gmail.com>
2015-11-08 23:25:44 +0900

    First commit.

---
+++ WebApplication/Controllers/FileController.cs
@@ -0,0 +1,65 @@
+﻿namespace WebApplication.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+    public class FileController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public async Task<HttpResponseMessage> Post(bool overwrite = false)
+        {
+            var tempPath = Path.GetTempPath();
+            var provider = new MultipartFormDataStreamProvider(tempPath);
+
+            await this.Request.Content.ReadAsMultipartAsync(provider);
+
+            foreach (var file in provider.FileData)
+            {
+                // アップロードファイル名の取得
+                var fileName = file.Headers.ContentDisposition.FileName;
+                fileName = fileName.StartsWith("\"") || fileName.StartsWith("'") ? fileName.Substring(1, fileName.Length - 1) : fileName;
+                fileName = fileName.EndsWith("\"") || fileName.EndsWith("'") ? fileName.Substring(0, fileName.Length - 1) : fileName;
+                fileName = Path.GetFileName(fileName);
+
+                // ファイルの移動
+                try
+                {
+                    File.Move(file.LocalFileName, Path.Combine("D:\\", fileName));
+                }
+                catch (Exception exception)
+                {
+                    return this.Request.CreateResponse(HttpStatusCode.OK, new { Message = exception.Message });
+                }
+            }
+
+            return this.Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication/Scripts/Home/index.js
@@ -0,0 +1,81 @@
+﻿/**
+* 指定されたファイルをアップロードします。
+* @param    {FileList}  files   アップロードするファイルリスト
+*/
+var upload = function (files) {
+    var i, formData;
+
+    // アップロード用のデータを生成
+    formData = new FormData();
+    for (i = files.length; i--;) {
+        formData.append('files', files[i]);
+    }
+
+    // ファイルアップロードの実行
+    $.ajax({
+        url: '/api/file?overwrite=true',
+        method: 'POST',
+        processData: false,
+        contentType: false,
+        data: formData
+    }).done(function (data, textStatus, jqXHR) {
+        $('#msg').append(JSON.stringify(data));
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        $('#msg').append(textStatus);
+    });
+};
+
+/**
+* 指定されたファイルを画面に表示する。
+* @param    {FileList}  files   読み取るファイルリスト
+*/
+var read = function (files) {
+    var fragment, i, item;
+    
+    fragment = document.createDocumentFragment();
+
+    for (i = files.length; i--;) {
+        item = document.createElement('div');
+        item.appendChild(document.createTextNode(files[i].name));
+        fragment.appendChild(item);
+    }
+
+    $('#msg').append(fragment);
+};
+
+/**
+* dragover イベント が発生したとき呼び出されます。
+* @param    {Event}     event   イベントオブジェクト
+*/
+var body_ondragover = function (event) {
+    event.preventDefault();
+    $('#msg').text('ondragover');
+};
+
+/**
+* drop イベント が発生したとき呼び出されます。
+* @param    {Event}     event   イベントオブジェクト
+*/
+var body_ondrop = function (event) {
+    var i, files, fragment, item;
+
+    $('#msg').text('ondrop');
+
+    files = event.originalEvent.dataTransfer.files || [];
+
+    read(files);
+    upload(files);
+};
+
+/**
+* ドキュメント生成が完了したとき呼び出されます。
+*/
+var document_onready = function (event) {
+    $(window.document.body).on(
+        'dragover', body_ondragover
+    ).on(
+        'drop', body_ondrop
+    );
+};
+
+$(document).ready(document_onready);
